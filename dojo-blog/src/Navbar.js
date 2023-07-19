ec55353a126dcf9c9fdec7afad9a31fedb5beeaf
Artur Zacniewski <a.zacniewski@gmail.com>
2023-07-19 11:27:27 +0200

    Add Link in the Navbar

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,14 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
         const title = 'The Dojo Blog';
-        const link = 'www.google.pl';
 
         return (
             <nav className="navbar">
                 <h1>{title}</h1>
                 <div className="links">
-                    <a href="/">Home</a>
-                    <a href="/create">New blog</a>
-                    <a href={link}>Google site</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/create">New blog</Link>
                 </div>
             </nav>
         );
