4a47aaf4508aeb483a79a5537bc80e436242ee6d
Artur Zacniewski <a.zacniewski@gmail.com>
2023-07-22 21:33:13 +0200

    BlogDetails - up to part #27

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
               <Routes>
                   <Route path="/"element={ <Home /> } />
                   <Route path="/create" element={ <Create /> } />
+                  <Route path="/blogs/:id" element={<BlogDetails /> } />
               </Routes>
           </div>
         </div>
