94170724c59916c37bcb2fe9168f18c385fbf211
Artur Zacniewski <a.zacniewski@gmail.com>
2023-07-24 16:48:21 +0200

    404 page

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,8 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
                   <Route path="/"element={ <Home /> } />
                   <Route path="/create" element={ <Create /> } />
                   <Route path="/blogs/:id" element={<BlogDetails /> } />
+                  <Route path="*" element={<NotFound /> } />
               </Routes>
           </div>
         </div>
