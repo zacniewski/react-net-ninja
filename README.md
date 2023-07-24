05491811537473ddf29c2b596c51aa7d70333f38
Artur Zacniewski <a.zacniewski@gmail.com>
2023-07-24 16:48:12 +0200

    Adding and deleting posts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Repo of [Full React Tutorial](https://www.youtube.com/watch?v=j942wKiXFu8&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d) by Net Ninja
+### Updated code for:  
+  - React v18,
+  - react-router-dom v6.
--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      // history.go(-1);
+      navigate('/');
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
